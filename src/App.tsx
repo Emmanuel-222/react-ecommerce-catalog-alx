54867faddf2824b71cbf2bca1a8e6ff4824ac2d5
Emmagodei <emmanuelogbuefi43@gmail.com>
2025-08-02 18:54:52 +0100

    feat: set up project structure with React and TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,52 @@
-// import { useState, useEffect } from "react";
-// import axios from "axios";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-// import type { Product } from "./interfaces";
-// import LoadingAnimation from "./components/LoadingAnimation";
+import type { Product } from "./interfaces";
+import LoadingAnimation from "./components/LoadingAnimation";
 
-// import ProductCatalogPage from "./pages/ProductCatalogPage";
-// import Navbar from "./components/Navbar";
+import ProductCatalogPage from "./pages/ProductCatalogPage";
+import Navbar from "./components/Navbar";
 
-// const App: React.FC = () => {
-//   const [products, setProducts] = useState<Product[]>([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [isError, setIsError] = useState(false);
+const App: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-//   // Navbar input
-//   const [searchFilter, setSearchFilter] = useState("");
+  // Navbar input
+  const [searchFilter, setSearchFilter] = useState("");
 
 
-//   // Fetch products from the API
-//   useEffect(() => {
-//     const fetchProducts = async () => {
-//       setIsLoading(true);
-//       setIsError(false);
+  // Fetch products from the API
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     setIsLoading(true);
+  //     setIsError(false);
 
-//       try {
-//         const response = await axios.get<Product[]>(
-//           "https://fakestoreapi.com/products"
-//         );
-//         setProducts(response.data);
-//         console.log(response.data);
-//       } catch (error) {
-//         console.error("The error is ", error);
-//         setIsError(true);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
+  //     try {
+  //       const response = await axios.get<Product[]>(
+  //         "https://fakestoreapi.com/products"
+  //       );
+  //       setProducts(response.data);
+  //       console.log(response.data);
+  //     } catch (error) {
+  //       console.error("The error is ", error);
+  //       setIsError(true);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   };
 
-//     fetchProducts();
-//   }, []); // Re-fetch products when searchFilter changes
+  //   fetchProducts();
+  // }, []); // Re-fetch products when searchFilter changes
 
-//   if (isLoading) return <LoadingAnimation />;
-//   if (isError) return <p>Something went wrong while fetching products.</p>;
-//   return (
-//     <>
-//       <Navbar searchFilter={searchFilter} setSearchFilter={setSearchFilter} />
-//       <ProductCatalogPage searchFilter={searchFilter} products={products} />
-//     </>
-//   );
-// };
+  // if (isLoading) return <LoadingAnimation />;
+  // if (isError) return <p>Something went wrong while fetching products.</p>;
+  return (
+    <>
+      <Navbar searchFilter={searchFilter} setSearchFilter={setSearchFilter} />
+      <ProductCatalogPage searchFilter={searchFilter} products={products} />
+    </>
+  );
+};
 
-// export default App;
+export default App;
