fc324837d8edc4004afc567289198cb6db463c38
Emmagodei <emmanuelogbuefi43@gmail.com>
2025-08-09 04:06:53 +0100

    Fixes: Update for pagination prev and next arrow

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -35,6 +35,7 @@ const Pagination: React.FC<PaginationProps> = ({
       <div className="flex items-center space-x-2">
         <button
           onClick={handlePrevPage}
+          disabled={currentPage <= 1}
           className="px-3 py-1 rounded-md border border-gray-300 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-gray-100"
         >
           <ArrowLeft className="" />
@@ -52,15 +53,10 @@ const Pagination: React.FC<PaginationProps> = ({
             {i + 1}
           </button>
         ))}
-        {/* <button className="px-3 py-1 rounded-md border border-gray-300 bg-indigo-600 text-white">
-          {currentPage}
-        </button> */}
-        {/* <button className="px-3 py-1 rounded-md border border-gray-300">
-          2
-        </button> */}
         <button
           onClick={handleNextPage}
-          className="px-3 py-1 rounded-md border border-gray-300 disabled:opacity-50 disabled:cursor-not-allowed hover:bg-gray-100"
+          disabled={currentPage >= totalPages}
+          className="px-3 py-1 rounded-md border border-gray-300 hover:bg-gray-100  disabled:opacity-50 disabled:cursor-not-allowed"
         >
           <ArrowRight className="" />
         </button>
